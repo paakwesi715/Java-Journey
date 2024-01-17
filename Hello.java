9a6774d8a4a1692ffd9faa2c2652dd722006b36a
paakwesi <brokingsley715@gmail.com>
2024-01-17 12:30:21 +0000

    my first  commit

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello 
+{
+    public static void main(String[] args) 
+     {
+        int Fnum = 10;
+        int Snum = 2;
+        int Product = Fnum * Snum ;
+        int Sum = Fnum + Snum ;
+        System.out.println(Product);
+        System.out.println(Sum);
+
+    }
+}
